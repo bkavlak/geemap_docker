449d1910805ef0a7a1b4020657dbb1b18aeb76a2
bkavlak <batuhankavlak4@gmail.com>
2021-07-25 15:27:58 +0300

    simplified Dockerfile

---
+++ install_dependencies.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "
+
+This script is for installing dependencies for up42-challenge
+
+"
+
+package_list=(
+    
+    build-essential
+    software-properties-common
+    python3.8
+    python3-tk
+    python3-pip
+    build-essential
+    libfreetype6-dev
+    libpng-dev
+    libzmq3-dev
+    libspatialindex-dev
+    libgl1-mesa-glx
+    gdal-bin
+    libgdal-dev
+    python3-gdal
+    libsm6
+    vim
+    wget
+    zip
+
+)
+
+apt-get update --fix-missing && apt-get install -y --no-install-recommends ${package_list[@]}
+apt-get clean && rm -rf /var/lib/apt/lists/*
+
